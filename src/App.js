998cc93b73c5b856b9bd27c387a7e5b7879f623d
zoeblighton <zoeblighton.seo@gmail.com>
2025-11-24 20:34:33 +0000

    Dictionary function created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import logo from "./dictionary-logo.png";
-
+import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
@@ -8,6 +8,10 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Zoe Blighton</footer>
       </div>
     </div>
   );
