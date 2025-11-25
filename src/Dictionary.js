610bf0ff4234071ff6471a3d1e9eb26a7b7368a9
zoeblighton <zoeblighton.seo@gmail.com>
2025-11-25 16:45:22 +0000

    Dictionary.js created

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary">Hello from Dictionary Component!</div>;
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert("Searching");
+  }
+
+  function handleKeywordChange(event) {
+    console.log(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          autoFocus={true}
+          placeholder="Search for a word..."
+          onChange={handleKeywordChange}
+        />
+      </form>
+    </div>
+  );
 }
